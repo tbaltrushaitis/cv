9232989ebf6be7615b1df240da76b6a425bf630e
Baltrushaitis Tomas <tbaltrushaitis@gmail.com>
2018-09-03 11:26:32 +0300

    Makefiles update

---
+++ bin/Colors.mk
@@ -0,0 +1,39 @@
+##  ------------------------------------------------------------------------  ##
+##        Colors definitions (taken from Color Bash Prompt HowTo)             ##
+##  ------------------------------------------------------------------------  ##
+
+##  Normal Colors
+Black=[0;1;30m
+Red=[0;1;31m
+Green=[0;1;32m
+Yellow=[0;1;33m
+Blue=[0;1;34m
+Purple=[0;1;35m
+Cyan=[0;1;36m
+White=[0;1;37m
+
+##  Bold
+BBlack=[1;1;30m
+BBlue=[1;1;34m
+BCyan=[1;1;36m
+BRed=[1;1;31m
+BGreen=[1;1;32m
+BYellow=[1;1;33m
+BPurple=[1;1;35m
+BWhite=[1;1;37m
+
+##  Background
+On_Red=[41m
+On_Green=[42m
+On_Yellow=[43m
+On_Blue=[44m
+On_Purple=[45m
+On_Cyan=[46m
+On_White=[47m
+On_Black=[40m
+
+##  Helpers
+NC=[m
+ALERT=${BWhite}${On_Red}
+
+##  ------------------------------------------------------------------------  ##
---
+++ bin/Utils.mk
@@ -0,0 +1,73 @@
+##  ------------------------------------------------------------------------  ##
+##                      Report Environment Variables                          ##
+##  ------------------------------------------------------------------------  ##
+
+include ./bin/.bash_colors
+
+##  ------------------------------------------------------------------------  ##
+
+.PHONY: state
+
+state:
+	@ echo ${BCyan}---------------------------------------------------------${NC};
+	@ echo ${BYellow}ENVIRONMENT VARS:${NC};
+	@ echo ${BCyan}APPLICATION:${NC};
+	@ echo "\t DT \t\t = ${DT}";
+	@ echo "\t APP_NAME \t = ${APP_NAME}";
+	@ echo "\t APP_ENV \t = ${APP_ENV}";
+	@ echo "\t APP_DEBUG \t = ${APP_DEBUG}";
+	@ echo "\t APP_LOGO \t = ${APP_LOGO}";
+	@ echo "\t APP_DIRS \t = [${APP_DIRS}]";
+	@ echo ${BCyan}REPOSITORY:${NC};
+	@ echo "\t APP_BRANCH \t = ${APP_BRANCH}";
+	@ echo "\t REPO_URL \t = ${REPO_URL}";
+	@ echo "\t GIT_COMMIT \t = ${GIT_COMMIT}";
+	@ echo ${BCyan}ENGINE:${NC};
+	@ echo "\t WD \t\t = ${WD}";
+	@ echo "\t DIR_SRC \t = ${DIR_SRC}";
+	@ echo ${BCyan}TARGETS:${NC};
+	@ echo "\t DIR_BUILD \t = ${DIR_BUILD}";
+	@ echo "\t DIR_DIST \t = ${DIR_DIST}";
+	@ echo "\t DIR_WEB \t = ${DIR_WEB}";
+	@ echo ${BCyan}---------------------------------------------------------${NC};
+
+##  ------------------------------------------------------------------------  ##
+##                 Lists all targets defined in this makefile                 ##
+##  ------------------------------------------------------------------------  ##
+
+.PHONY: list
+list:
+	@$(MAKE) -pRrn : -f $(MAKEFILE_LIST) 2>/dev/null | awk -v RS= -F: '/^# File/,/^# Finished Make data base/ {if ($$1 !~ "^[#.]") {print $$1}}' | egrep -v -e '^[^[:alnum:]]' -e '^$@$$' | sort
+
+##  ------------------------------------------------------------------------  ##
+##                          Show project's banner                             ##
+##  ------------------------------------------------------------------------  ##
+
+.PHONY: banner
+
+banner:
+	@ if [ -f "${APP_LOGO}" ]; then cat "${APP_LOGO}"; fi
+
+##  ------------------------------------------------------------------------  ##
+
+##  ------------------------------------------------------------------------  ##
+##                 Set/fix files and dirs owners and permissions              ##
+##  ------------------------------------------------------------------------  ##
+
+.PHONY: rights
+
+rights:
+	@ find . -type f -exec chmod 664 {} 2>/dev/null \;
+	@ find . -type d -exec chmod 775 {} 2>/dev/null \;
+	@ find . -type f -name "*.sh" -exec chmod a+x {} 2>/dev/null \;
+
+##  ------------------------------------------------------------------------  ##
+
+.PHONY: clone
+
+clone:
+	@  git clone ${APP_REPO} ${APP_NAME} \
+	&& cd ${APP_NAME} \
+	&& git pull;
+
+	##  ------------------------------------------------------------------------  ##
