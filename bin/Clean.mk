037ea7991fde88a86aa6f9198e0898afaa7ad005
Baltrushaitis Tomas <tbaltrushaitis@gmail.com>
2019-02-18 03:50:05 +0200

    Improved logging

--- bin/Clean.mk
+++ bin/Clean.mk
@@ -2,38 +2,50 @@
 ##                                 Clean Environment                          ##
 ##  ------------------------------------------------------------------------  ##
 
-.PHONY: clean clean-all clean-deps
-.PHONY: clean-build clean-dist clean-files
+include bin/.bash_colors
 
-clean-all: clean clean-dist
-	@ echo "[DONE] Clean all";
+.PHONY: clean clean-all clean-full
+.PHONY: clean-build clean-dist clean-files clean-deps
 
 clean: clean-build clean-files
-	@ echo "[DONE] Clean";
+	@ echo [${BYellow}${On_Blue}COMPLETED${NC}] Clean;
+
+clean-all: clean clean-dist
+	@ echo [${BYellow}${On_Blue}COMPLETED${NC}] Clean ALL;
+
+clean-full: clean-all clean-deps
+	@ echo [${BYellow}${On_Blue}COMPLETED${NC}] Clean FULL;
 
 clean-build:
-	@ echo "Clean: build ... ";
-	@ rm -rf ${DIR_BUILD}
+	@ echo -n "Clean: build \t ... ";
+	@ rm -rf ${DIR_BUILD} ;
+	@ echo [${White}OK${NC}];
 
 clean-dist:
-	@ echo "Clean: distro ... ";
-	@ rm -rf ${DIR_DIST}
+	@ echo -n "Clean: dist \t ... ";
+	@ rm -rf ${DIR_DIST} ;
+	@ echo [${White}OK${NC}];
 
 clean-deps:
-	@ echo "Clean: deps ... ";
+	@ echo -n "Clean: deps \t ... ";
 	@ rm -rf \
 		bower_modules/ \
 		node_modules/ ;
+	@ echo [${White}OK${NC}];
 
 clean-files:
-	@ echo "Clean: files ... ";
+	@ echo -n "Clean: files \t ... ";
 	@ rm -rf BUILD-* \
 		COMMIT \
 		*.md \
+		package-lock.json \
+		.bowerrc \
+		.npmrc \
 		bitbucket-pipelines.yml \
 		codeclimate-config.patch \
 		_config.yml \
 		yarn.lock \
 		yarn-error.log ;
+	@ echo [${White}OK${NC}];
 
 ##  ------------------------------------------------------------------------  ##
