ce1e9f97765db43eb18377252596dce2a1f5594f
Baltrushaitis Tomas <tbaltrushaitis@gmail.com>
2019-02-18 03:52:05 +0200

    Improved logging

--- bin/Utils.mk
+++ bin/Utils.mk
@@ -2,11 +2,7 @@
 ##                             Utility Helpers                                ##
 ##  ------------------------------------------------------------------------  ##
 
-.ONESHELL:
-
-# include ./bin/.bash_colors
-# include bin/Colors
-include bin/.bash_colors
+# include bin/.bash_colors
 
 ##  ------------------------------------------------------------------------  ##
 ##                      Report Environment Variables                          ##
@@ -16,24 +12,24 @@ include bin/.bash_colors
 
 state:
 	@ echo ${BCyan}---------------------------------------------------------${NC};
-	@ echo ${Orange}${On_Blue}ENVIRONMENT VARS:${NC};
-	@ echo ${BCyan}APPLICATION:${NC};
-	@ echo "\t DT \t\t = ${DT}";
-	@ echo "\t APP_NAME \t = ${APP_NAME}";
-	@ echo "\t APP_ENV \t = ${BRed}${APP_ENV}${NC}";
-	@ echo "\t APP_DEBUG \t = ${APP_DEBUG}";
-	@ echo "\t APP_LOGO \t = ${APP_LOGO}";
-	@ echo ${BCyan}REPOSITORY:${NC};
-	@ echo "\t APP_BRANCH \t = ${APP_BRANCH}";
-	@ echo "\t APP_REPO \t = ${APP_REPO}";
-	@ echo "\t GIT_COMMIT \t = ${Yellow}${GIT_COMMIT}${NC}";
-	@ echo ${BCyan}ENGINE:${NC};
-	@ echo "\t WD \t\t = ${WD}";
-	@ echo "\t DIR_SRC \t = ${DIR_SRC}";
-	@ echo ${BCyan}TARGETS:${NC};
-	@ echo "\t DIR_BUILD \t = ${DIR_BUILD}";
-	@ echo "\t DIR_DIST \t = ${DIR_DIST}";
-	@ echo "\t DIR_WEB \t = ${DIR_WEB}";
+	@ echo ${BYellow}${On_Blue}ENVIRONMENT VARS:${NC} ;
+	@ echo ${BCyan}APPLICATION:${NC} ;
+	@ echo "\t DT \t\t = "${DT} ;
+	@ echo "\t APP_NAME \t = "${APP_NAME} ;
+	@ echo "\t APP_ENV \t = "${BRed}${APP_ENV}${NC} ;
+	@ echo "\t APP_LOGO \t = "${Blue}${APP_LOGO}${NC} ;
+	@ echo "\t APP_DEBUG \t = "${APP_DEBUG} ;
+	@ echo ${BCyan}REPOSITORY:${NC} ;
+	@ echo "\t APP_REPO \t = "${White}${APP_REPO}${NC} ;
+	@ echo "\t GIT_COMMIT \t = "${Yellow}${GIT_COMMIT}${NC} ;
+	@ echo "\t APP_BRANCH \t = "${APP_BRANCH} ;
+	@ echo ${BCyan}ENGINE:${NC} ;
+	@ echo "\t WD \t\t = "${WD} ;
+	@ echo "\t DIR_SRC \t = "${DIR_SRC} ;
+	@ echo ${BCyan}TARGETS:${NC} ;
+	@ echo "\t DIR_BUILD \t = "${DIR_BUILD} ;
+	@ echo "\t DIR_DIST \t = "${DIR_DIST} ;
+	@ echo "\t DIR_WEB \t = "${DIR_WEB} ;
 	@ echo ${BCyan}---------------------------------------------------------${NC};
 
 
@@ -52,6 +48,7 @@ banner:
 ##  ------------------------------------------------------------------------  ##
 
 .PHONY: list
+
 list:
 	@$(MAKE) -pRrn : -f $(MAKEFILE_LIST) 2>/dev/null | awk -v RS= -F: '/^# File/,/^# Finished Make data base/ {if ($$1 !~ "^[#.]") {print $$1}}' | egrep -v -e '^[^[:alnum:]]' -e '^$@$$' | sort
 
