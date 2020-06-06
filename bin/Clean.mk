0d4af2f0d584f6739de8d8f9ce6450a4438c6ea5
Baltrushaitis Tomas <tbaltrushaitis@gmail.com>
2020-06-07 00:54:16 +0300

    Upgrade 'clean' make-task

--- bin/Clean.mk
+++ bin/Clean.mk
@@ -3,7 +3,7 @@
 ##  ------------------------------------------------------------------------  ##
 
 THIS_FILE = $(lastword $(MAKEFILE_LIST))
-# $(info [THIS_FILE:$(THIS_FILE)])
+$(info $(DAT) [THIS_FILE:$(THIS_FILE)])
 
 .PHONY: clean clean-all clean-full
 .PHONY: clean-build clean-dist clean-files clean-deps
@@ -15,25 +15,25 @@ clean-all: clean clean-dist ;
 	@ echo "$(DAT) $(FINE): $(TARG)" ;
 
 clean-full: clean-all clean-build clean-deps ;
-	@ rm -rf build-* ;
-	@ rm -rf web-* ;
+	rm -rf build-* ;
+	rm -rf web-* ;
 	@ echo "$(DAT) $(FINE): $(TARG)" ;
 
 clean-build: ;
 	@ echo -n "Clean: build \t ... ";
-	@ rm -rf ${DIR_BUILD} ;
-	@ rm -f build ;
+	rm -rf ${DIR_BUILD} ;
+	rm -f build ;
 	@ echo "$(OKAY)" ;
 
 clean-dist: ;
 	@ echo -n "Clean: dist \t ... ";
-	@ rm -rf ${DIR_DIST} ;
-	@ rm -f dist ;
+	rm -rf ${DIR_DIST} ;
+	rm -f dist ;
 	@ echo "$(OKAY)" ;
 
 clean-deps: ;
 	@ echo -n "Clean: deps \t ... ";
-	@ rm -rf 						\
+	rm -rf 							\
 		bower_modules/ 		\
 		node_modules/ 		\
 		package-lock.json \
@@ -43,7 +43,7 @@ clean-deps: ;
 
 clean-files: ;
 	@ echo -n "Clean: files \t ... ";
-	@ rm -rf COMMIT							\
+	rm -rf COMMIT								\
 		bitbucket-pipelines.yml		\
 		codeclimate-config.patch	\
 		_config.yml								\
