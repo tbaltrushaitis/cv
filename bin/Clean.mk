0b6b3f2857a586ef017c6d2e38772e8a83663e46
tbaltrushaitis <tbaltrushaitis@gmail.com>
2022-09-30 02:30:03 +0300

    Update additional Makefiles

--- bin/Clean.mk
+++ bin/Clean.mk
@@ -1,12 +1,11 @@
 ##  ------------------------------------------------------------------------  ##
 ##                                 Clean Environment                          ##
 ##  ------------------------------------------------------------------------  ##
-
 THIS_FILE = $(lastword $(MAKEFILE_LIST))
 # $(info $(DAT) [THIS_FILE:$(THIS_FILE)])
 
 .PHONY: clean clean-all clean-full
-.PHONY: clean-build clean-dist clean-files clean-deps
+.PHONY: clean-build clean-dist clean-deps clean-files
 
 clean: clean-build clean-files ;
 	@ echo "$(DAT) $(FINE): $(TARG)" ;
@@ -14,46 +13,47 @@ clean: clean-build clean-files ;
 clean-all: clean clean-dist ;
 	@ echo "$(DAT) $(FINE): $(TARG)" ;
 
-clean-full: clean-all clean-build clean-deps ;
+clean-full: clean-build clean-deps clean-all;
+	rm -rf dist-* ;
 	rm -rf build-* ;
-	rm -rf web-* ;
 	@ echo "$(DAT) $(FINE): $(TARG)" ;
 
 clean-build: ;
-	@ echo -n "Clean: build \t ... ";
+	@ echo -n "Clean: build \t ... " ;
 	rm -rf ${DIR_BUILD} ;
-	rm -f build ;
+	rm -f ./build ;
 	@ echo "$(OKAY)" ;
 
 clean-dist: ;
-	@ echo -n "Clean: dist \t ... ";
+	@ echo -n "Clean: dist \t ... " ;
 	rm -rf ${DIR_DIST} ;
-	rm -f dist ;
+	rm -f ./dist ;
 	@ echo "$(OKAY)" ;
 
 clean-deps: ;
-	@ echo -n "Clean: deps \t ... ";
-	rm -rf 							\
-		bower_modules/ 		\
-		node_modules/ 		\
-		package-lock.json \
-		setup-deps				\
-		setup ;
+	@ echo -n "Clean: deps \t ... " ;
+	rm -rf 						\
+		bower_modules/ 	\
+		node_modules/ 	\
+		./setup-deps		\
+	;
 	@ echo "$(OKAY)" ;
 
 clean-files: ;
-	@ echo -n "Clean: files \t ... ";
-	rm -rf COMMIT								\
-		bitbucket-pipelines.yml		\
-		codeclimate-config.patch	\
-		_config.yml								\
-		yarn.lock									\
-		*.md											\
-		*.log											\
-		deploy ;
+	@ echo -n "Clean: files \t ... " ;
+	rm -rf COMMIT							\
+		bitbucket-pipelines.yml	\
+		package-lock.json 			\
+		_config.yml							\
+		*.lock									\
+		*.md										\
+		*.log										\
+		./deploy								\
+	;
 	@ echo "$(OKAY)" ;
 
 clean-dev: clean-files clean-build clean-dist ;
+	@ echo -n "Clean: dev \t ... " ;
 	@ echo "$(DAT) $(DONE): $(TARG)" ;
 
 ##  ------------------------------------------------------------------------  ##
